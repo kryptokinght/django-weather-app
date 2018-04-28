958faa9bf53e3ff68633e537b30419a3405092a6
Minanshu Singh <minanshu.1998@gmail.com>
2018-04-29 03:59:32 +0530

    added functionality for displaying weather of multiple cities

--- weather/models.py
+++ weather/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+	city = models.CharField(max_length=200)
+	def __str__(self):
+		return self.city
+
+	class Meta:
+		verbose_name_plural = 'cities'
--- weather/views.py
+++ weather/views.py
@@ -3,19 +3,28 @@ from __future__ import unicode_literals
 
 import requests
 from django.shortcuts import render
+from .models import City
 
 # Create your views here.
 
 def index(request):
 	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=450ccb44bf951d091d75ae77946a0c26'
-	city = 'Kolkata'
-	res = requests.get(url.format(city)).json()
-	city_weather = {
-		"city":city,
-		"temp":res['main']['temp'] - 273,
-		"desc":res['weather'][0]['description'],
-		"icon":res['weather'][0]['icon']
-	}
-	context = {'city_weather': city_weather}
-	print (city_weather)
+	
+	cities = City.objects.all()
+	cities_weather = []
+	
+	for city in cities:
+		res = requests.get(url.format(city)).json()
+		city_weather = {
+			"city":city,
+			"temp":res['main']['temp'] - 273,
+			"desc":res['weather'][0]['description'],
+			"icon":res['weather'][0]['icon']
+		}
+		cities_weather.append(city_weather)
+
+	print(cities_weather);
+	
+	
+	context = {'cities_weather': cities_weather}
 	return render(request, 'weather.html', context)
