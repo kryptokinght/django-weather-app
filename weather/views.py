36be77a420a3b81cf16358c9b60a9055f351dd08
Minanshu Singh <minanshu.1998@gmail.com>
2018-04-29 04:09:35 +0530

    form data gets saved in database

--- weather/views.py
+++ weather/views.py
@@ -2,29 +2,33 @@
 from __future__ import unicode_literals
 
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import City
 
 # Create your views here.
 
 def index(request):
-	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=450ccb44bf951d091d75ae77946a0c26'
-	
-	cities = City.objects.all()
-	cities_weather = []
-	
-	for city in cities:
-		res = requests.get(url.format(city)).json()
-		city_weather = {
-			"city":city,
-			"temp":res['main']['temp'] - 273,
-			"desc":res['weather'][0]['description'],
-			"icon":res['weather'][0]['icon']
-		}
-		cities_weather.append(city_weather)
+	if(request.method == 'POST'):
+		cityName = request.POST['cityName']
 
-	print(cities_weather);
-	
-	
-	context = {'cities_weather': cities_weather}
-	return render(request, 'weather.html', context)
+		city = City(city=cityName)
+		city.save()
+		return redirect('/')
+	else:
+		url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=450ccb44bf951d091d75ae77946a0c26'
+		
+		cities = City.objects.all()
+		cities_weather = []
+		
+		for city in cities:
+			res = requests.get(url.format(city)).json()
+			city_weather = {
+				"city":city,
+				"temp":res['main']['temp'] - 273,
+				"desc":res['weather'][0]['description'],
+				"icon":res['weather'][0]['icon']
+			}
+			cities_weather.append(city_weather)
+		
+		context = {'cities_weather': cities_weather}
+		return render(request, 'weather.html', context)
